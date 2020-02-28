5e41428c97b5d077d64d46dea2e5900c8e257afc
Habtamu <myabiti@gmail.com>
2020-02-28 05:25:33 +0400

    components part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import Dishdetail from './DIshdetailComponent';
+import { View } from 'react-native';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            selectedDish: null
+        };
+    }
 
-  render() {
- 
-    return (
-        <Menu dishes={this.state.dishes} />
-    );
-  }
+    onDishSelect(dishId) {
+        this.setState({ selectedDish: dishId })
+    }
+
+    render() {
+
+        return (
+            <View style={{ flex: 1 }}>
+                <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
+                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+            </View>
+        );
+    }
 }
-  
+
 export default Main;
