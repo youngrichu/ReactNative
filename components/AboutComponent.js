4753c5cd3e8efdaf4b9183c1f1814d5f54a31765
Habtamu <myabiti@gmail.com>
2020-03-10 04:02:29 +0400

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import { FlatList, ScrollView, Text, View } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { LEADERS } from '../shared/leaders';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        leaders: state.leaders
+    }
+}
 
 function History(props) {
-    return(
+    return (
         <Card title='Our History'>
-            <Text style={{margin: 10, fontSize: 16}}>
-            Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.{"\n\n"}
-            The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
+            <Text style={{ margin: 10, fontSize: 16 }}>
+                Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.{"\n\n"}
+                The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
            </Text>
         </Card>
     );
@@ -18,43 +26,35 @@ function CorporateLeadership(props) {
     const leaders = props.leaders;
 
     if (leaders != null) {
-        const renderLeader = ({item, index}) => {
-            return(
+        const renderLeader = ({ item, index }) => {
+            return (
                 <ListItem
                     key={index}
                     title={item.name}
-                    titleStyle={{fontWeight: 'bold'}}
+                    titleStyle={{ fontWeight: 'bold' }}
                     subtitle={item.description}
-                    subtitleStyle={{color: '#6c757d'}} // #6c757d taken from Bootstrap blockquote-footer CSS
+                    subtitleStyle={{ color: '#6c757d' }} // #6c757d taken from Bootstrap blockquote-footer CSS
                     hideChevron={true}
-                    leftAvatar={{ source: require('./images/alberto.png')}}
-                    />
+                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
+                />
             );
         };
-        return(
+        return (
             <Card title='Corporate Leadership'>
                 <FlatList
-                    data={leaders}
+                    data={this.props.leaders.leaders}
                     renderItem={renderLeader}
                     keyExtractor={item => item.id.toString()}
-                    />                
+                />
             </Card>
         );
     }
     else {
-        return(<View></View>)
+        return (<View></View>)
     }
 }
 
-class About extends Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-            leaders: LEADERS
-        };
-    }
-
+class About extends Component {
     static navigationOptions = {
         title: 'About Us'
     };
@@ -63,13 +63,13 @@ class About extends Component{
 
 
 
-        return(
+        return (
             <ScrollView>
                 <History />
-                <CorporateLeadership leaders={this.state.leaders}/>
+                <CorporateLeadership leaders={this.state.leaders} />
             </ScrollView>
         );
     };
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -7,7 +7,24 @@ import About from './AboutComponent';
 import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    }
+}
+
+const mapDispatchToProps = dispatch => ({
+    fetchDishes: () => dispatch(fetchDishes()),
+    fetchComments: () => dispatch(fetchComments()),
+    fetchPromos: () => dispatch(fetchPromos()),
+    fetchLeaders: () => dispatch(fetchLeaders()),
+})
 
 const MenuNavigator = createStackNavigator({
     Menu: {
@@ -124,12 +141,12 @@ const MainNavigator = createDrawerNavigator({
             drawerLabel: 'Menu',
             drawerIcon: ({ tintColor, focused }) => (
                 <Icon
-                  name='list'
-                  type='font-awesome'            
-                  size={24}
-                  color={tintColor}
+                    name='list'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
                 />
-              ),
+            ),
         }
 
     },
@@ -172,7 +189,12 @@ console.ignoredYellowBox = ['Warning:'];
 /*---------- /--------------*/
 class Main extends Component {
 
-
+    componentDidMount() {
+        this.props.fetchDishes();
+        this.props.fetchComments();
+        this.props.fetchPromos();
+        this.props.fetchLeaders();
+    }
     render() {
 
         return (
@@ -207,4 +229,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default Main;
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
